6a82e7cfee8e88e0fe2d80d2b646ba3c30e01989
Huang, Weiting <weiting.huang@gatech.edu>
2023-10-14 17:28:45 -0400

    Permission request launches

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation ("androidx.fragment:fragment-ktx:1.3.0-rc01")
     implementation ("androidx.activity:activity:1.2.0-rc01")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation ("com.google.android.gms:play-services-location:18.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
