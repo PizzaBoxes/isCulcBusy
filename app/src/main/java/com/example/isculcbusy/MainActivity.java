997f0b36adf5c500c801d7f4ae3c7e6829425718
Huang, Weiting <weiting.huang@gatech.edu>
2023-10-14 16:27:25 -0400

    adding permission stuff

--- app/src/main/java/com/example/isculcbusy/MainActivity.java
+++ app/src/main/java/com/example/isculcbusy/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.isculcbusy;
 
+import android.Manifest;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,6 +18,31 @@ import com.example.isculcbusy.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    private ActivityResultLauncher<String[]> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+                    result -> {
+                        Boolean fineLocationGranted = false;
+                        if (android.Manifest.permission.ACCESS_FINE_LOCATION != null) {
+                            fineLocationGranted = true;
+                        }
+                        Boolean coarseLocationGranted = false;
+                        if (Manifest.permission.ACCESS_COARSE_LOCATION != null) {
+                            coarseLocationGranted = true;
+                        }
+                        if (fineLocationGranted && coarseLocationGranted) {
+                            // Precise location access granted.
+                        } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                            // Only approximate location access granted.
+                        } else {
+                            // No location access granted.
+                        }
+                    }
+            );
+
+    public void launch(ActivityResultLauncher<String[]> requestPermissionLauncher) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +61,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
+
+
 
 }
